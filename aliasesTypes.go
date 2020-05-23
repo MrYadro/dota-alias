156f7e6b35d6420bba3e5f343665b9384b4109f1
Yaroslav Yadrishnikov <y.yadrushnikov@corp.vk.com>
2020-05-23 17:36:54 +0300

    fix team

--- aliasesTypes.go
+++ aliasesTypes.go
@@ -4,3 +4,11 @@ type Aliases []struct {
 	ID      int      `json:"id"`
 	Aliases []string `json:"aliases"`
 }
+
+type AliasesList []AliasesListItem
+
+type AliasesListItem struct {
+	EntityType string   `json:"type"`
+	ID         int      `json:"id"`
+	Aliases    []string `json:"aliases"`
+}
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const (
@@ -34,31 +34,24 @@ func loadJSON(path string, target interface{}) error {
 	return nil
 }
 
+func findInJSON(aliases Aliases, id int) []string {
+	var out []string
+	for _, alias := range aliases {
+		if alias.ID == id {
+			out = alias.Aliases
+		}
+	}
+	return out
+}
+
 func main() {
 	var players ODotaProPlayers
 	var teams ODotaTeams
 	var leagues ODotaLeagues
 
-	var dict string
-
 	getJSON(playersURL, &players)
 	getJSON(teamsURL, &teams)
 	getJSON(leaguesURL, &leagues)
-
-	for _, player := range players {
-		dictEl := fmt.Sprintf("%s,%s\n", player.Personaname, playerTag)
-		dict += dictEl
-	}
-
-	for _, team := range teams {
-		dictEl := fmt.Sprintf("%s,%s\n", team.Name, teamTag)
-		dict += dictEl
-	}
-
-	for _, league := range leagues {
-		dictEl := fmt.Sprintf("%s,%s\n", league.Name, leagueTag)
-		dict += dictEl
-	}
 
 	var playersAliases Aliases
 	var teamsAliases Aliases
@@ -68,42 +61,47 @@ func main() {
 	loadJSON("./aliases/teams.json", &teamsAliases)
 	loadJSON("./aliases/leagues.json", &leaguesAliases)
 
-	for _, player := range playersAliases {
-		for _, playerAlias := range player.Aliases {
-			dictEl := fmt.Sprintf("%s,%s\n", playerAlias, playerTag)
-			dict += dictEl
+	textAliases := AliasesList{}
+
+	for _, player := range players {
+		pID := player.AccountID
+		pName := strings.ToLower(player.Name)
+		pAliases := findInJSON(playersAliases, pID)
+		pAliases = append(pAliases, pName)
+		alias := AliasesListItem{
+			ID:         pID,
+			EntityType: playerTag,
+			Aliases:    pAliases,
 		}
+		textAliases = append(textAliases, alias)
 	}
 
-	for _, team := range teamsAliases {
-		for _, teamAlias := range team.Aliases {
-			dictEl := fmt.Sprintf("%s,%s\n", teamAlias, teamTag)
-			dict += dictEl
+	for _, team := range teams {
+		tID := team.TeamID
+		tName := strings.ToLower(team.Name)
+		tAliases := findInJSON(teamsAliases, tID)
+		tAliases = append(tAliases, tName)
+		alias := AliasesListItem{
+			ID:         tID,
+			EntityType: teamTag,
+			Aliases:    tAliases,
 		}
+		textAliases = append(textAliases, alias)
 	}
 
-	for _, league := range leaguesAliases {
-		for _, leagueAlias := range league.Aliases {
-			dictEl := fmt.Sprintf("%s,%s\n", leagueAlias, leagueTag)
-			dict += dictEl
+	for _, league := range leagues {
+		lID := league.Leagueid
+		lName := strings.ToLower(league.Name)
+		lAliases := findInJSON(leaguesAliases, lID)
+		lAliases = append(lAliases, lName)
+		alias := AliasesListItem{
+			ID:         lID,
+			EntityType: leagueTag,
+			Aliases:    lAliases,
 		}
+		textAliases = append(textAliases, alias)
 	}
 
-	f, err := os.Create("dict.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	l, err := f.WriteString(dict)
-	if err != nil {
-		fmt.Println(err)
-		f.Close()
-		return
-	}
-	fmt.Println(l, "bytes written successfully")
-	err = f.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	jsonString, _ := json.Marshal(textAliases)
+	ioutil.WriteFile("dict.json", jsonString, os.ModePerm)
 }
